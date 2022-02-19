4fa379ae317ba8f252dae5754a6dd4c65634405b
Ioannis Mertzanis <imertz@protonmail.com>
2022-02-19 02:52:08 +0200

    Add hall of fame

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var ghpages = require('gh-pages');
 
 let today = new Date();
 today.setHours(today.getHours() + 2);
-let rawdata = fs.readFileSync(__dirname+'/data.json');
+let rawdata = fs.readFileSync(__dirname + '/data.json');
 let student = JSON.parse(rawdata);
 
 const content = `<!DOCTYPE html>
@@ -31,31 +31,42 @@ const content = `<!DOCTYPE html>
           <h1 class="card__title">MySolonBot</h1>
           <h2 class="card__subtitle">Statistics</h2>
           <p>
-            <em>Χρήστες:</em> ${student.users} <br /><em>Υποθέσεις:</em> ${student.cases}<br /><em
-              >Τελευταίος έλεγχος:</em
-            >
+            🤷🏻<em> Χρήστες:</em> ${student.users} <br />📁<em> Υποθέσεις:</em>
+            ${student.cases} <br />
+            <br />
+            <strong>🏛Hall Of Shame🏛</strong> <br />
+            Παλαιότερη δικάσιμος χωρίς εκδοθείσα απόφαση:<br />
+            🥇 ${(new Date(student.res.noDec[0].imerominia)).toLocaleDateString('el-GR')} - ${student.res.noDec[0].court}<br />
+            🥈 ${(new Date(student.res.noDec[1].imerominia)).toLocaleDateString('el-GR')} - ${student.res.noDec[1].court} <br />🥉 ${(new Date(student.res.noDec[2].imerominia)).toLocaleDateString('el-GR')} - ${student.res.noDec[2].court}<br /><br />
+            Παλαιότερη κατάθεση χωρίς εκδοθείσα απόφαση: <br />
+            🥇 ${(new Date(student.res.sinceKatathesi[0].katathesi)).toLocaleDateString('el-GR')} - ${student.res.sinceKatathesi[0].court}<br />
+            🥈 ${(new Date(student.res.sinceKatathesi[1].katathesi)).toLocaleDateString('el-GR')} - ${student.res.sinceKatathesi[1].court}<br />🥉 ${(new Date(student.res.sinceKatathesi[2].katathesi)).toLocaleDateString('el-GR')} - ${student.res.sinceKatathesi[2].court}<br /><br />
+            Μεγαλύτερη απόσταση μεταξύ κατάθεσης και δικασίμου <br/>
+            🥇 ${(new Date(student.res.biggestDif[0].katathesi)).toLocaleDateString('el-GR')} - ${(new Date(student.res.biggestDif[0].imerominia)).toLocaleDateString('el-GR')}  ${student.res.biggestDif[0].court}<br />
+            🥈 ${(new Date(student.res.biggestDif[1].katathesi)).toLocaleDateString('el-GR')} - ${(new Date(student.res.biggestDif[1].imerominia)).toLocaleDateString('el-GR')}  ${student.res.biggestDif[1].court}<br />🥉 ${(new Date(student.res.biggestDif[2].katathesi)).toLocaleDateString('el-GR')} - ${(new Date(student.res.biggestDif[2].imerominia)).toLocaleDateString('el-GR')}  ${student.res.biggestDif[2].court}<br />
+            <br />🕰<em> Τελευταίος έλεγχος:</em>
             ${today.toLocaleString('el-GR')}
-          </p>
-        </section>
-      </main>
-    </div>
-    <script>
+          </p >
+        </section >
+      </main >
+    </div >
+  <script>
       document.addEventListener("DOMContentLoaded", () => {
-        // Initialising the script with the Project ID
-        // you can find in the dashboard
-        swetrix.init("9j4Gy9gAGcgQ");
+      // Initialising the script with the Project ID
+      // you can find in the dashboard
+      swetrix.init("9j4Gy9gAGcgQ");
 
-        // Tracking page views
-        swetrix.trackViews();
+    // Tracking page views
+    swetrix.trackViews();
       });
-    </script>
-  </body>
-</html>
+  </script>
+  </body >
+</html >
 
   `
 
 try {
-  fs.writeFileSync(__dirname+"/src/index.html", content)
+  fs.writeFileSync(__dirname + "/src/index.html", content)
   //file written successfully
 } catch (err) {
   console.error(err)
@@ -64,6 +75,6 @@ try {
 
 
 
-ghpages.publish(__dirname+"/src", function(err) {console.log('ho')});
+ghpages.publish(__dirname + "/src", function (err) { console.log('ho') });
 
 
